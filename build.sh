3f5688a674101ceee3adc75341a9d0fc5cf050ea
root <root@localhost.localdomain>
2014-12-15 21:35:47 +0800

    Modify build.sh dup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ CATALINA_HOME=`echo ${CATALINA_HOME}`
 echo "[COBBLE.GE] Stop tomcat..."
 sh ${CATALINA_HOME}/bin/shutdown.sh
 # bak upload dir
-echo "[COBBLE.GE] Bak [upload] directory..."
+## echo "[COBBLE.GE] Bak [upload] directory..."
 ## /bin/cp -R ${HS_BASE}/target/huasheng/upload/* ${HS_BASE}/temp/upload/
 
 echo "[COBBLE.GE] cd ${HS_BASE} ..."
@@ -17,7 +17,7 @@ mvn clean install -Dmaven.test.skip=true
 /bin/cp -f ${HS_BASE}/target/takeaway/WEB-INF/classes/jdbc.release.properties ${HS_BASE}/target/takeaway/WEB-INF/classes/jdbc.properties
 /bin/cp -Rf ${HS_BASE}/target/takeaway /opt/app/www
 # Restore upload dir
-echo "[COBBLE.GE] Restore [upload] directory..."
+## echo "[COBBLE.GE] Restore [upload] directory..."
 ## mkdir -p ${HS_BASE}/target/huasheng/upload
 ## /bin/cp -R ${HS_BASE}/temp/upload/* ${HS_BASE}/target/huasheng/upload/
 echo "[COBBLE.GE] Start tomcat..."
