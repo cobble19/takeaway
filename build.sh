bb6872467de89aa9ab7e32c46757bc08b3d272a6
Cobble.Ge.Computer <publicczhzh@sina.com>
2015-01-31 19:35:59 +0800

    Add comment...

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 . /etc/profile
 HS_BASE=/opt/app/takeaway
 CATALINA_HOME=`echo ${CATALINA_HOME}`
+echo "[COBBLE.GE] Stop MySQL..."
+service mysql stop
 echo "[COBBLE.GE] Stop tomcat..."
 sh ${CATALINA_HOME}/bin/shutdown.sh
 # bak upload dir
@@ -14,15 +16,22 @@ echo "[COBBLE.GE] Start git pull..."
 git pull 
 echo "[COBBLE.GE] Start mvn..."
 mvn clean install -Dmaven.test.skip=true
+echo "[COBBLE.GE] Copy jdbc.properties"
 /bin/cp -f ${HS_BASE}/target/takeaway/WEB-INF/classes/jdbc.release.properties ${HS_BASE}/target/takeaway/WEB-INF/classes/jdbc.properties
+echo "cd ${HS_BASE}/target/takeaway/mgr/ta ..."
 cd ${HS_BASE}/target/takeaway/mgr/ta
+echo "Ext sencha app build ..."
 sencha app build
+echo "[COBBL.GE] Copy ${HS_BASE}/target/takeaway/mgr/ta/build/production/TA/* ${HS_BASE}/target/takeaway/mgr/ta ..."
 /bin/cp -Rf ${HS_BASE}/target/takeaway/mgr/ta/build/production/TA/* ${HS_BASE}/target/takeaway/mgr/ta
+echo "[COBBLE.GE]Copy ${HS_BASE}/target/takeaway /opt/app/www  ..."
 /bin/cp -Rf ${HS_BASE}/target/takeaway /opt/app/www
 # Restore upload dir
 ## echo "[COBBLE.GE] Restore [upload] directory..."
 ## mkdir -p ${HS_BASE}/target/huasheng/upload
 ## /bin/cp -R ${HS_BASE}/temp/upload/* ${HS_BASE}/target/huasheng/upload/
+echo "[COBBLE.GE] Start MySQL..."
+service mysql start
 echo "[COBBLE.GE] Start tomcat..."
 sh ${CATALINA_HOME}/bin/startup.sh
 echo "[COBBLE.GE] Build finished..."
