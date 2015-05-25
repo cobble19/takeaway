da01e8361da06eceaddd22754c5fa459a33e099f
Cobble.Ge.Computer <publicczhzh@sina.com>
2015-05-26 00:12:41 +0800

    computer

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+. /etc/profile
+HS_BASE=/opt/app/takeaway
+
+echo "[COBBLE.GE] cd ${HS_BASE} ..."
+cd ${HS_BASE}
+
+echo "[COBBLE.GE] Start git fetch & merge..."
+git fetch origin
+git merge origin refs/heads/master -m "git merge from github"
+echo "[COBBLE.GE] Finish git fetch & merge..."
+
+echo "[COBBLE.GE] Start git push ... "
+git push origin refs/heads/master
+echo "[COBBLE.GE] Finish git push ..."
