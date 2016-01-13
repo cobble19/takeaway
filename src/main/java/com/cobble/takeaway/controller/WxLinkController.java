8660b06be289f0b7de57f96329763f498ac4c57d
bange <bange@cisco.com>
2016-01-14 00:58:10 +0800

    1. replace ,html 127.0.0.1->request.servername

--- src/main/java/com/cobble/takeaway/controller/WxLinkController.java
+++ src/main/java/com/cobble/takeaway/controller/WxLinkController.java
@@ -174,7 +174,7 @@ public class WxLinkController extends BaseController {
 			
 			fromUrl += "?wxTemplateId=" + wxTemplateId + "&userId=" + userId;
 			
-			ret = FileUtil.url2Html(fromUrl, toFileFullPath);
+			ret = FileUtil.url2Html(fromUrl, toFileFullPath, request.getServerName());
 			
 			String htmlPath = "files/htmls" + "/" + toFilePath;
 			htmlPath = htmlPath.replace("\\", "/");
--- src/main/java/com/cobble/takeaway/util/FileUtil.java
+++ src/main/java/com/cobble/takeaway/util/FileUtil.java
@@ -18,6 +18,35 @@ public class FileUtil {
 		HtmlConvertedPOJO ret = null;
 		try {
 			String resp = HttpClientUtil.get(fromFullUrl);
+			
+			toFileFullPath = toFileFullPath.replace("/", File.separator);
+			
+			logger.info("fromFullUrl: {}, toFileFullPath: {}", fromFullUrl, toFileFullPath);
+			
+			File dest = new File(toFileFullPath);
+			if (!dest.exists()) {
+				dest.createNewFile();
+			}
+			FileUtils.writeStringToFile(dest, resp, "UTF-8");
+			ret = new HtmlConvertedPOJO();
+			ret.setSuccess(true);
+			ret.setFromFullUrl(fromFullUrl);
+			ret.setToFilePath(toFileFullPath);
+		} catch (Exception e) {
+			logger.error("toHtml error: {}", e);
+			ret.setSuccess(false);
+		}
+		
+		return ret;
+	}
+	
+	public static HtmlConvertedPOJO url2Html(String fromFullUrl, String toFileFullPath, String serverName) {
+		HtmlConvertedPOJO ret = null;
+		try {
+			String resp = HttpClientUtil.get(fromFullUrl);
+			
+			resp = resp.replace("127.0.0.1", serverName);
+			
 			toFileFullPath = toFileFullPath.replace("/", File.separator);
 			
 			logger.info("fromFullUrl: {}, toFileFullPath: {}", fromFullUrl, toFileFullPath);
