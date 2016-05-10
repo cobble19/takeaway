65c90501dc4a25e22540f83d1dc9f02b458b6f3f
bange <bange@cisco.com>
2016-05-10 10:59:26 +0800

    1. XML <-> POJO convert

--- src/main/java/com/cobble/takeaway/pojo/WxComVerifyTicketPOJO.java
+++ src/main/java/com/cobble/takeaway/pojo/WxComVerifyTicketPOJO.java
@@ -1,9 +1,11 @@
 package com.cobble.takeaway.pojo;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name="xml")
 public class WxComVerifyTicketPOJO extends BasePOJO {
 	@XmlElement(name="AppId")
---
+++ src/main/java/com/cobble/takeaway/util/XmlUtils.java
@@ -0,0 +1,65 @@
+package com.cobble.takeaway.util;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cobble.takeaway.pojo.WxComVerifyTicketPOJO;
+
+public class XmlUtils {
+	private static Logger logger = LoggerFactory.getLogger(XmlUtils.class);
+	
+	public static String convertToXml(Object obj) {
+		return convertToXml(obj, "UTF-8", false, true);
+	}
+	
+	public static String convertToXml(Object obj, Boolean formatted) {
+		return convertToXml(obj, "UTF-8", formatted, true);
+	}
+
+	public static String convertToXml(Object obj, String encoding, Boolean formatted, Boolean fragment) {
+		String result = null;
+		try {
+			JAXBContext context = JAXBContext.newInstance(obj.getClass());
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, formatted);
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, encoding);
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, fragment);// whether remove xml head
+
+			StringWriter writer = new StringWriter();
+			marshaller.marshal(obj, writer);
+			result = writer.toString();
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	public static <T> T convertToJavaBean(String xml, Class<T> c) {
+		T t = null;
+		try {
+			JAXBContext context = JAXBContext.newInstance(c);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			t = (T) unmarshaller.unmarshal(new StringReader(xml));
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return t;
+	}
+	public static void main(String[] args) {
+		WxComVerifyTicketPOJO wxComVerifyTicketPOJO = new WxComVerifyTicketPOJO();
+		wxComVerifyTicketPOJO.setAppId("appid1233");
+		wxComVerifyTicketPOJO.setComponentVerifyTicket("abc");
+		wxComVerifyTicketPOJO.setCreateTime("1233456");
+		wxComVerifyTicketPOJO.setInfoType("1");
+		String result = XmlUtils.convertToXml(wxComVerifyTicketPOJO, true);
+		logger.info(result);
+	}
+}
