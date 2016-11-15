7336c76d734094820d8e37e7e6af6aae628a8090
Cobble.Ge.Computer <publicczhzh@sina.com>
2016-11-15 21:42:21 +0800

    From Compute, zzd.

--- bakMySQL.sh
+++ bakMySQL.sh
@@ -3,6 +3,7 @@
 # bak MySQL db   #
 ##################
 # 1. cp origin huasheng.sql
-cp takeawaydb.sql takeawaydb.sql.bak
-mysqldump --socket=/tmp/mysql.sock -c -uroot -proot123 --default-character-set=utf8 takeawaydb > takeawaydb.sql
+dateStr=`date +%y%m%d`
+#cp takeawaydb.sql takeawaydb.sql.$dateStr.bak
+mysqldump --socket=/tmp/mysql.sock -c -uroot -proot123 --default-character-set=utf8 takeawaydb > takeawaydb.$dateStr.sql
 
